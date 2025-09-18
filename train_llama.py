e5e9082b50dec98518cc4b66cd2759d3c82cfda5
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2025-09-18 12:33:35 -0400

    rm single gpu code

--- train_llama.py
+++ train_llama.py
@@ -2,11 +2,8 @@
 FROM https://github.com/karpathy/llama2.c/blob/master/train.py
      https://github.com/karpathy/llama2.c/blob/master/model.py
 
-This training script can be run both on a single gpu in debug mode,
-and also in a larger training run with distributed data parallel (ddp).
-
-To run on a single GPU small debug run, example:
-$ python -m train.py --compile=False --eval_iters=10 --batch_size=8
+This training script is designed for distributed data parallel (ddp) training
+across multiple GPUs and nodes.
 
 To run with DDP on 4 gpus on 1 node, example:
 $ torchrun --standalone --nproc_per_node=4 train.py
@@ -491,25 +488,19 @@ assert vocab_source in ["llama2", "custom"]
 assert vocab_source == "custom" or vocab_size == 32000, "The vocab from Meta has 32K tokens"
 
 # various inits, derived attributes, I/O setup
-ddp = int(os.environ.get("RANK", -1)) != -1  # is this a ddp run?
-if ddp:
-    init_process_group(backend="nccl")
-    ddp_rank = int(os.environ["RANK"])
-    ddp_local_rank = int(os.environ["LOCAL_RANK"])
-    ddp_world_size = int(os.environ["WORLD_SIZE"])
-    device = f"cuda:{ddp_local_rank}"
-    torch.cuda.set_device(device)
-    master_process = ddp_rank == 0  # this process will do logging, checkpointing etc.
-    seed_offset = ddp_rank  # each process gets a different seed
-    # world_size number of processes will be training simultaneously, so we can scale
-    # down the desired gradient accumulation iterations per process proportionally
-    assert gradient_accumulation_steps % ddp_world_size == 0
-    gradient_accumulation_steps //= ddp_world_size
-else:
-    # if not ddp, we are running on a single gpu, and one process
-    master_process = True
-    seed_offset = 0
-    ddp_world_size = 1
+# always expect DDP environment variables to be set
+init_process_group(backend="nccl")
+ddp_rank = int(os.environ["RANK"])
+ddp_local_rank = int(os.environ["LOCAL_RANK"])
+ddp_world_size = int(os.environ["WORLD_SIZE"])
+device = f"cuda:{ddp_local_rank}"
+torch.cuda.set_device(device)
+master_process = ddp_rank == 0  # this process will do logging, checkpointing etc.
+seed_offset = ddp_rank  # each process gets a different seed
+# world_size number of processes will be training simultaneously, so we can scale
+# down the desired gradient accumulation iterations per process proportionally
+assert gradient_accumulation_steps % ddp_world_size == 0
+gradient_accumulation_steps //= ddp_world_size
 tokens_per_iter = gradient_accumulation_steps * ddp_world_size * batch_size * max_seq_len
 if master_process:
     print(f"tokens per iteration will be: {tokens_per_iter:,}")
@@ -530,12 +521,8 @@ ptdtype = {
 ctx = nullcontext() if device_type == "cpu" else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
 # data loader setup
-if ddp:
-    train_loader = DistributedDataLoader(train_data_path, batch_size, max_seq_len, ddp_rank, ddp_world_size)
-    val_loader = DistributedDataLoader(val_data_path, batch_size, max_seq_len, ddp_rank, ddp_world_size)
-else:
-    train_loader = DistributedDataLoader(train_data_path, batch_size, max_seq_len, 0, 1)
-    val_loader = DistributedDataLoader(val_data_path, batch_size, max_seq_len, 0, 1)
+train_loader = DistributedDataLoader(train_data_path, batch_size, max_seq_len, ddp_rank, ddp_world_size)
+val_loader = DistributedDataLoader(val_data_path, batch_size, max_seq_len, ddp_rank, ddp_world_size)
 
 # init these up here, can override if init_from='resume' (i.e. from a checkpoint)
 iter_num = 0
@@ -606,12 +593,11 @@ if compile:
     model = torch.compile(model)  # requires PyTorch 2.0
 
 # wrap model into DDP container
-if ddp:
-    # Ignore the `freqs_cis` buffer so that DDP does not broadcast it at
-    # construction time since NCCL does not support `ComplexFloat`
-    prefix = "_orig_mod." if compile else ""
-    model._ddp_params_and_buffers_to_ignore = {prefix + "freqs_cis"}
-    model = DDP(model, device_ids=[ddp_local_rank])
+# Ignore the `freqs_cis` buffer so that DDP does not broadcast it at
+# construction time since NCCL does not support `ComplexFloat`
+prefix = "_orig_mod." if compile else ""
+model._ddp_params_and_buffers_to_ignore = {prefix + "freqs_cis"}
+model = DDP(model, device_ids=[ddp_local_rank])
 
 
 # helps estimate an arbitrarily accurate loss over either split using many batches
@@ -654,7 +640,7 @@ def get_lr(it):
 X, Y = train_loader.next_batch()  # fetch the very first batch
 t0 = time.time()
 local_iter_num = 0  # number of iterations in the lifetime of this process
-raw_model = model.module if ddp else model  # unwrap DDP container if needed
+raw_model = model.module  # unwrap DDP container
 running_mfu = -1.0
 while True:
     # determine and set the learning rate for this iteration
@@ -684,12 +670,11 @@ while True:
     # forward backward update, with optional gradient accumulation to simulate larger batch size
     # and using the GradScaler if data type is float16
     for micro_step in range(gradient_accumulation_steps):
-        if ddp:
-            # in DDP training we only need to sync gradients at the last micro step.
-            # the official way to do this is with model.no_sync() context manager, but
-            # I really dislike that this bloats the code and forces us to repeat code
-            # looking at the source of that context manager, it just toggles this variable
-            model.require_backward_grad_sync = micro_step == gradient_accumulation_steps - 1
+        # in DDP training we only need to sync gradients at the last micro step.
+        # the official way to do this is with model.no_sync() context manager, but
+        # I really dislike that this bloats the code and forces us to repeat code
+        # looking at the source of that context manager, it just toggles this variable
+        model.require_backward_grad_sync = micro_step == gradient_accumulation_steps - 1
         with ctx:
             logits = model(X, Y)
             loss = raw_model.last_loss
@@ -726,5 +711,4 @@ while True:
     if iter_num > max_iters:
         break
 
-if ddp:
-    destroy_process_group()
+destroy_process_group()
